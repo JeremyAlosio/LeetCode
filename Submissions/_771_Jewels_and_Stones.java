ce3bedb32af86966e03089729fcc753f1a05a263
Jeremy Alosio <Jeremy.Alosio@gmail.com>
2019-01-16 12:02:27 -0500

    Completed 771

--- Submissions/_771_Jewels_and_Stones.java
+++ Submissions/_771_Jewels_and_Stones.java
@@ -1,4 +1,24 @@
+import java.util.HashSet;
+
+//https://leetcode.com/problems/jewels-and-stones/
+// #771
+// Easy
 
 public class _771_Jewels_and_Stones {
-	
+	public int numJewelsInStones(String J, String S) {
+		int totalJewels = 0;
+
+		HashSet<Character> diamondsInTheRough = new HashSet<Character>();
+
+		for (Character c : J.toCharArray())
+			diamondsInTheRough.add(c);
+
+		for (Character c : S.toCharArray()) {
+			if (diamondsInTheRough.contains(c)) {
+				totalJewels++;
+			}
+		}
+
+		return totalJewels;
+	}
 }
